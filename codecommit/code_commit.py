19959873e1c2949c3111b2c4ae1c0e70af7b1601
david <ddmc@gftdevgcp.com>
2019-05-19 22:42:10 +0100

    moving data to class rather than main

--- codecommit/code_commit.py
+++ codecommit/code_commit.py
@@ -1,8 +1,12 @@
 import boto3
 import pyboto3
+from src.codecommit.client_locator import CODECOMMITClient
 
 # A class for putting, commiting and deleting files from AWS code commit
 class codeCommit:
+    parent_commit_id = ""
+    commits = 0
+
     def __init__(self, client):
         self._client = client
         """ :type : pyboto3.codecommit"""
@@ -58,6 +62,7 @@ class codeCommit:
                 },
             ]
         )
+
     # A method to put a file, unfortunately needs the parent commit id so not great
     def put_file(self, repo_name, branch_name, file_content, file_path, cid, commit_message, author_name, author_email):
         return self._client.put_file(
@@ -71,9 +76,47 @@ class codeCommit:
             name=author_name,
             email=author_email
         )
+
     # A method to create a repo
     def create_repository(self, repo_name, repo_description):
         return self._client.create_repository(
             repositoryName=repo_name,
             repositoryDescription=repo_description
         )
+
+    # a method to setup the initial repo, create the file and some logic
+    def post_data_to_repo(self, username, data_to_post, commits):
+        author_name = username
+        author_email = "Billy@Bob.com"
+        branch = "master"
+        repo = "terraform"
+        file_path = "terraform/request.json"
+        commit_message = "Terraform commit"
+        ccommit_client = CODECOMMITClient().get_client()
+        cclient = codeCommit(ccommit_client)
+        # if commits = 0 then create a repo and the initial file
+        # If the repo is already in use or created this will fail as commit ID can only be empty in an empty repo
+        if (commits == 0):
+            try:
+                # create terraform repo
+                repos = cclient.create_repository(repo, "A repo for Terraform request response data")
+                print("terraform repo created")
+                # class calls method and assigns the api response
+                commit_response = cclient.commit_file_new_repo(repo, branch, author_name, author_email, file_path,
+                                                               data_to_post, commit_message)
+                print("great this worked so repo must have been created with an empty directory")
+                # get the commit ID of the first commit
+                codeCommit.parent_commit_id = commit_response['commitId']
+                codeCommit.commits += 1
+            except:
+                print("directory already exists or is not an empty directory")
+        # if this is the second commit we can use the commit ID
+        else:
+            try:
+                # Add the file using the cid (commit id) of the put file
+                commit_response = cclient.commit_file(codeCommit.parent_commit_id, repo, branch, author_name, author_email, file_path,
+                                                      data_to_post, commit_message)
+                codeCommit.parent_commit_id = commit_response['commitId']
+                codeCommit.commits += 1
+            except:
+                print("we should not get here")
--- deploy.py
+++ deploy.py
@@ -2,47 +2,12 @@ from src.codecommit.code_commit import codeCommit
 from src.codecommit.client_locator import CODECOMMITClient
 
 def main():
-    #TODO: drop all of this code into a method in the code_commit.py class
-    #TODO: add the below variables into the class so they are stored in the object
-    #TODO: exception handling and unit tests
-    author_name = "David"
-    author_email = "anemail"
-    branch = "master"
-    repo = "terraform"
-    file_path = "terraform/request.json"
-    commits = 0
-    commit_message = "Terraform commit"
+
+    username = "David"
+    data = "some jason data to post"
     ccommit_client = CODECOMMITClient().get_client()
     cclient = codeCommit(ccommit_client)
-    file_content = "something here"
-    pcid = ""
-
-
-    # if commits = 0 then create a repo and the initial file
-    # If the repo is already in use or created this will fail as commit ID can only be empty in an empty repo
-
-    if (commits == 0):
-        try:
-            #create terraform repo
-            repos = cclient.create_repository(repo, "A repo for Terraform request response data")
-            print("terraform repo created")
-            # class calls method and assigns the api response
-            commit_response = cclient.commit_file_new_repo(repo, branch, author_name, author_email, file_path, file_content, commit_message)
-            print("great this worked so repo must have been created with an empty directory")
-            # get the commit ID of the first commit
-            pcid = commit_response['commitId']
-            commits += 1
-        except:
-            print("directory already exists or is not an empty directory")
-            print(pcid)
-
-    # if this is the second commit we can use the commit ID
-    else:
-        # Add the file using the cid (commit id) of the put file
-        commit_response = cclient.commit_file(pcid, repo, branch, author_name, author_email, file_path, file_content, commit_message)
-        pcid = commit_response['commitId']
-        commits += 1
-
+    response = cclient.post_data_to_repo(username, data)
 
 if __name__ == '__main__':
     main()
