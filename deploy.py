ad1272061ac0bcc199b2d10c6bde991d3329e0f4
david <ddmc@gftdevgcp.com>
2019-05-19 22:52:39 +0100

    re factoring

--- deploy.py
+++ deploy.py
@@ -2,7 +2,7 @@ from src.codecommit.code_commit import codeCommit
 from src.codecommit.client_locator import CODECOMMITClient
 
 def main():
-
+    #TODO: exception handling and testing, run final tests after Friday's re factoring
     username = "David"
     data = "some jason data to post"
     ccommit_client = CODECOMMITClient().get_client()
