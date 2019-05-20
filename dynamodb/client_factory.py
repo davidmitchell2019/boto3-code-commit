333714031f1f2b4c6ee0becb1845826553f91728
david <ddmc@gftdevgcp.com>
2019-05-20 10:07:04 +0100

    adding comments and a TODO

--- dynamodb/client_factory.py
+++ dynamodb/client_factory.py
@@ -3,8 +3,11 @@ import boto3
 
 class ClientFactory:
     def __init__(self, client):
-        self._client = boto3.client(client, region_name="eu-west-3")
-
+        self._client = boto3.client(client, region_name="eu-west-2")
+        # aws_access_key_id = ACCESS_KEY
+        # aws_secret_access_key = SECRET_KEY
+        # TODO: pass access key and secret key into client locator for authentication
+        
     def get_client(self):
         return self._client
 
--- dynamodb/dynamodb.py
+++ dynamodb/dynamodb.py
@@ -3,6 +3,7 @@ class DynamoDB:
         self._client = client
         """ :type : pyboto3.dynamodb """
 
+    # create a DynamoDB table
     def create_table(self, table, attribute_definitions, key_schema, iops):
         print("Creating DynamoDB table...")
         return self._client.create_table(
@@ -12,10 +13,12 @@ class DynamoDB:
             ProvisionedThroughput=iops
         )
 
+    #Describe a DynamoDB table
     def describe_table(self, table):
         print("Describing DynamoDB table with name " + table)
         return self._client.describe_table(TableName=table)
 
+    #Update DynamoDB table read/write capacity
     def update_read_write_capacity(self, table_name, new_read_capacity, new_write_capacity):
         print("Updating Provisioned Throughput of table with name " + table_name)
         return self._client.update_table(
@@ -26,6 +29,7 @@ class DynamoDB:
             }
         )
 
+    #Delete a DynamoDB table
     def delete_table_with_name(self, table_name):
         print("Deleting DynamoDB table with name " + table_name)
         return self._client.delete_table(TableName=table_name)
